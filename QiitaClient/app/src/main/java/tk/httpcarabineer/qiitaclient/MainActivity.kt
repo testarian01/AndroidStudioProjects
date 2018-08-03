2bf46e3fd8d0b755e9172f0df9fdbcf1fc1b7e83
yano <yano@vixen.co.jp>
2018-08-03 18:37:17 +0900

    test

--- QiitaClient/app/src/main/java/tk/httpcarabineer/qiitaclient/MainActivity.kt
+++ QiitaClient/app/src/main/java/tk/httpcarabineer/qiitaclient/MainActivity.kt
@@ -1,10 +1,14 @@
-//249Pまで
+//P254まで
 
 package tk.httpcarabineer.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import sample.qiitaclient.view.ArticleView
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DialogTitle
+import android.widget.ListView
+import sample.qiitaclient.view.ArticleListAdapter
+import tk.httpcarabineer.qiitaclient.Article
+import tk.httpcarabineer.qiitaclient.User
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,18 +16,61 @@ class MainActivity : AppCompatActivity() {
 //        super.onCreate(savedInstanceState)
 //        setContentView(R.layout.view_article)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        //ArticleViewオブジェクトを生成
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("テスト", "たろうさん"),
+                dummyArticle("テスト", "じろうさん"))
 
-        //Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
-        articleView.setArticle(Article(id="123",
-                title = "Kotlin入門",
-                url = "http://www.example.com/articles/123",
-                user = User(id="456",name="たろう",profileImageUrl = "")))
-
-        //このアクテビティにArticleViewオブジェクトをセット
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    //ダミー記事を生成するメソッド
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
+
+//        //ArticleViewオブジェクトを生成
+//        val articleView = ArticleView(applicationContext)
+//
+//        //Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+//        articleView.setArticle(Article(id="123",
+//                title = "Kotlin入門",
+//                url = "http://www.example.com/articles/123",
+//                user = User(id="456",name="たろう",profileImageUrl = "")))
+//
+//        //このアクテビティにArticleViewオブジェクトをセット
+//        setContentView(articleView)
+
 }
+
+//package tk.httpcarabineer.qiitaclient
+//
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//import sample.qiitaclient.view.ArticleView
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+////        super.onCreate(savedInstanceState)
+////        setContentView(R.layout.view_article)
+//        super.onCreate(savedInstanceState)
+//
+//        //ArticleViewオブジェクトを生成
+//        val articleView = ArticleView(applicationContext)
+//
+//        //Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+//        articleView.setArticle(Article(id="123",
+//                title = "Kotlin入門",
+//                url = "http://www.example.com/articles/123",
+//                user = User(id="456",name="たろう",profileImageUrl = "")))
+//
+//        //このアクテビティにArticleViewオブジェクトをセット
+//        setContentView(articleView)
+//    }
+//}
 
