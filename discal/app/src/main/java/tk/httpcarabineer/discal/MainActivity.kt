8fff15b3dcde01b51f31e4a63787269a4b02a6a4
yano <yano@vixen.co.jp>
2018-08-22 11:42:46 +0900

    1.2

--- discal/app/src/main/java/tk/httpcarabineer/discal/MainActivity.kt
+++ discal/app/src/main/java/tk/httpcarabineer/discal/MainActivity.kt
@@ -3,15 +3,12 @@ package tk.httpcarabineer.discal
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.IntegerRes
 import android.view.View
 import android.widget.*
-import kotlinx.android.synthetic.*
 
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,33 +26,43 @@ class MainActivity : AppCompatActivity() {
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
 
-        //選択ボタン 1 ---ここから
+        //計算用ボタン
         val button_1 = findViewById<Button>(R.id.button_1)
-
-
-
-
-        //選択ボタン ％引------------------------------------------------------
-        val button_0 = findViewById<Button>(R.id.button_0)
+        val button_2 = findViewById<Button>(R.id.button_2)
 
         //計算用ボタン
         val b_keisann_1 = findViewById<Button>(R.id.b_keisann_1)
+        val b_keisann_2 = findViewById<Button>(R.id.b_keisann_2)
 
         //％引
         val et_offprice = findViewById<TextView>(R.id.et_offprice)
         val tx_offprice = findViewById<TextView>(R.id.tx_offprice)
 
-        //％引非表示
+        //割引
+        val et_offprice2 = findViewById<TextView>(R.id.et_offprice2)
+        val tx_offprice2 = findViewById<TextView>(R.id.tx_offprice2)
+
+        //％引・割引 非表示
         et_offprice.visibility = View.INVISIBLE
         tx_offprice.visibility = View.INVISIBLE
         b_keisann_1.visibility = View.INVISIBLE
 
-        button_0.setOnClickListener {
+        et_offprice2.visibility = View.INVISIBLE
+        tx_offprice2.visibility = View.INVISIBLE
+        b_keisann_2.visibility = View.INVISIBLE
 
-            //％引表示
+        //選択ボタン1------------------------------------------------------
+        button_1.setOnClickListener {
+
+            //％引 表示
             et_offprice.visibility = View.VISIBLE
             tx_offprice.visibility = View.VISIBLE
             b_keisann_1.visibility = View.VISIBLE
+
+            //割引 非表示
+            et_offprice2.visibility = View.INVISIBLE
+            tx_offprice2.visibility = View.INVISIBLE
+            b_keisann_2.visibility = View.INVISIBLE
 
             //計算ボタンをクリックしたら
             b_keisann_1.setOnClickListener {
@@ -103,14 +110,81 @@ class MainActivity : AppCompatActivity() {
                     //非表示に戻す
                     et_offprice.visibility = View.INVISIBLE
                     tx_offprice.visibility = View.INVISIBLE
+                    b_keisann_1.visibility = View.INVISIBLE
 
                     //ResultActivityの呼び出し
                     startActivity(intent)
 
                 }
             }
-        }//選択ボタン ％引 ここまで------------------------------------------------------
+        }
 
+        //選択ボタン2------------------------------------------------------
+        button_2.setOnClickListener {
 
+            //割引 表示
+            et_offprice2.visibility = View.VISIBLE
+            tx_offprice2.visibility = View.VISIBLE
+            b_keisann_2.visibility = View.VISIBLE
+
+            //％引 非表示
+            et_offprice.visibility = View.INVISIBLE
+            tx_offprice.visibility = View.INVISIBLE
+            b_keisann_1.visibility = View.INVISIBLE
+
+            //計算ボタンをクリックしたら
+            b_keisann_2.setOnClickListener {
+
+                //未入力判定のフラグ
+                var isValid = true
+                //金額入力欄の値を取得
+                val get_price = findViewById<EditText>(R.id.et_price)
+                val st_price = get_price.text.toString()
+                //合計金額が空欄になっていないかチェック
+                if(st_price.isEmpty()){
+                    get_price.error = getString(R.string.et_price_error)
+                    isValid = false
+                }
+
+                //オフ率
+                val get_offprice = findViewById<EditText>(R.id.et_offprice2)
+                val st_offprice = get_offprice.text.toString()
+                //オフ率が空欄になっていないかをチェック
+                if(st_offprice.isEmpty()) {
+                    //オフ率が未入力の場合
+                    get_offprice.error = getString(R.string.et_offprice2_error)
+                    isValid = false
+                }
+
+                //金額入力欄、オフ率入力欄に値が入っている場合
+                if(isValid) {
+                    //計算
+                    val goukei_price:Float = st_price.toFloat()
+                    val off_price:Float = st_offprice.toFloat()
+                    val keisan:Float = goukei_price*((10-off_price)/10)
+                    val kekka = keisan.toInt()
+                    val kekka_zeikomi_keisan:Float = (keisan*1.08).toFloat()
+                    val kekka_zeikomi = kekka_zeikomi_keisan.toInt()
+
+                    //ResultActivityに値を引き継ぎ
+                    val intent = Intent(this, ResultActivity::class.java)
+                    intent.putExtra("kekka", kekka)
+                    intent.putExtra("kekka_zeikomi", kekka_zeikomi)
+
+                    //金額・オフ率のEditTextを初期化
+                    get_price.text.clear()
+                    get_offprice.text.clear()
+
+                    //非表示に戻す
+                    et_offprice2.visibility = View.INVISIBLE
+                    tx_offprice2.visibility = View.INVISIBLE
+                    b_keisann_2.visibility = View.INVISIBLE
+
+                    //ResultActivityの呼び出し
+                    startActivity(intent)
+
+                }
+            }
+        }
     }
 }
--- discal/app/src/main/java/tk/httpcarabineer/discal/ResultActivity.kt
+++ discal/app/src/main/java/tk/httpcarabineer/discal/ResultActivity.kt
@@ -28,10 +28,6 @@ class ResultActivity : AppCompatActivity() {
         val kekka = intent.getIntExtra("kekka", 0)
         val kekka_zeikomi = intent.getIntExtra("kekka_zeikomi", 0)
 
-        //1人分の支払い金額と端数の計算
-//        val kekka = (goukei - tyousei) / ninnzuu
-//        val hasuu = (goukei - tyousei) % ninnzuu
-
         //１人分の支払い金額を表示
         //計算結果を表示
                 val set_kekka = findViewById<TextView>(R.id.tv_kekka_disp)
@@ -41,24 +37,9 @@ class ResultActivity : AppCompatActivity() {
                 val set_kekka_zeikomi = findViewById<TextView>(R.id.tv_kekka_zeikomi_disp)
                 set_kekka_zeikomi.text = getString(R.string.set_kekka_zeikomi,kekka_zeikomi)
 
-
-
-//        val set_kekka1 = findViewById<TextView>(R.id.tv_kekka1_disp)
-//        set_kekka1.text = Html.fromHtml(getString(R.string.set_kekka1, kekka))
-//
-//        if(hasuu != 0) {
-//            //端数が0の場合に表示
-//            val set_hasuu_ari = findViewById<TextView>(R.id.tv_kekka2_disp)
-//            set_hasuu_ari.text = Html.fromHtml(getString(R.string.set_hasuu_ari, hasuu))
-//
-//        }else{
-//            //端数が0以外なら端数の金額を表示
-//            val set_hasuu_nasi = findViewById<TextView>(R.id.tv_kekka2_disp)
-//            set_hasuu_nasi.text = getString(R.string.set_hasuu_nasi)
-//        }
-
         //もう１回計算ボタンをタップした時の処理
         val restart = findViewById<Button>(R.id.b_restart)
+
         restart.setOnClickListener {
             //ResultActivityを終了させる
             finish()
