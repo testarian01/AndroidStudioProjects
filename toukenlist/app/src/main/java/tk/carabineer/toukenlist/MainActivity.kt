abc86a337f9bf0cfbc8efaa12ade18cb61c59641
testarian01 <tes675816@gmail.com>
2018-09-10 22:43:53 +0900

    20180910

--- toukenlist/app/src/main/java/tk/carabineer/toukenlist/MainActivity.kt
+++ toukenlist/app/src/main/java/tk/carabineer/toukenlist/MainActivity.kt
@@ -19,15 +19,15 @@ class MainActivity : AppCompatActivity() {
         //        fun main(args: Array<String>) {
         // パースするJSON
 //        val json = "{ \"bunrui\": \"短刀\", \"name\": \"三日月\", \"nameyomi\": \"アイゼンクニトシ\", \"bikou\": \"重要文化財、銘 国俊。法人蔵\", \"bunrui2\": \"短刀\", \"name2\": \"三日月2\", \"nameyomi2\": \"アイゼンクニトシ\", \"bikou2\": \"重要文化財、銘 国俊。法人蔵\" }"
-        val json ="{ \"code\": { \"bunrui\": \"短刀\", \"name\": \"三日月\", \"nameyomi\": \"アイゼンクニトシ\", \"bikou\": \"重要文化財、銘 国俊。法人蔵\" }, \"code\": { \"bunrui\": \"短刀2\", \"name\": \"三日月2\", \"nameyomi\": \"アイゼンクニトシ2\", \"bikou\": \"重要文化財、銘 国俊。法人蔵2\" } }"
+        val json ="{ \"list01\": { \"bunrui\": \"短刀\", \"name\": \"三日月\", \"nameyomi\": \"アイゼンクニトシ\", \"bikou\": \"重要文化財、銘 国俊。法人蔵\" }, \"list02\": { \"bunrui\": \"短刀2\", \"name\": \"三日月2\", \"nameyomi\": \"アイゼンクニトシ2\", \"bikou\": \"重要文化財、銘 国俊。法人蔵2\" } }"
 
         // mapperオブジェクトを作成
         val mapper = jacksonObjectMapper()
 
         // jsonをdeserialize
         // 下の場合はjsonがColor型のオブジェクトにマッピングされる
-//        val color = mapper.readValue<Color>(json)
         val toukenlist = mapper.readValue<Toukenlist>(json)
+
 
 //        println(color)
 //        println("名前：${name.kana}")
@@ -37,14 +37,10 @@ class MainActivity : AppCompatActivity() {
 
 
         // リストの項目となる文字列を配列で設定
-        val items = arrayOf("名前：${toukenlist.code.name}")
-
-//        val items = arrayOf("テスト","テストだよ")
-
+        val items = arrayOf(toukenlist.list01.name, toukenlist.list02.name)
 
         // 配列から ArrayAdapter を作成
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
-
 
         // レイアウトXMLから ListView を読み込み、リスト項目を設定
         val listView: ListView = findViewById(R.id.listView)
--- toukenlist/app/src/main/java/tk/carabineer/toukenlist/Toukenlist.kt
+++ toukenlist/app/src/main/java/tk/carabineer/toukenlist/Toukenlist.kt
@@ -1,14 +1,21 @@
 package tk.carabineer.toukenlist
 
 data class Toukenlist(
-        val code: Code
+        val list01: list01,
+        val list02: list02
 )
 
-data class Code(
+data class list01(
         val bunrui:String,
         val name:String,
         val nameyomi:String,
         val bikou:String
 )
 
+data class list02(
+        val bunrui:String,
+        val name:String,
+        val nameyomi:String,
+        val bikou:String
+)
 
